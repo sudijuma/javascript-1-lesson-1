0b75a283babf0027d8b0fa12f4a037afc8bcc670
sudijuma <91537436+sudijuma@users.noreply.github.com>
2022-01-24 11:02:34 +0100

    x

---
+++ js/script.js
@@ -0,0 +1,41 @@
+var myName = "sudi";
+function login(arg) {
+    var myName = arg //Local scope
+    return myName;
+}
+var someVar = login("mwakie");
+console.log(someVar);
+
+//Example 2, callback functions
+
+var enterSite = function () {
+    console.log("hello you entered the website")
+}
+
+function loginToSite(arg) {
+    arg();
+}
+loginToSite(enterSite);
+
+var login = function (name){
+    console.log("you may login: ", name)
+}
+login("Sudi");
+
+function login (name){
+    console.log("you may login: ", name)
+}
+
+// function as a property of an object
+
+var user = {
+    name: "Sudi",
+    occupation: "student",
+    login: function (name){
+        console.log("hello, my name is", name)
+    }
+}
+console.log("my user object", user)
+console.log("name", user.name);
+
+user.login("Sudi")
